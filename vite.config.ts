df6e5c8dd0e2cba561f20581b335e45f838c3c15
AMIT KUMAR SAH <amitkumarsahugupta@gmail.com>
2026-06-22 00:25:48 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,17 +5,23 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+    host: '0.0.0.0', // Yeh Render ko port detect karne me madad karega
+    port: process.env.PORT ? parseInt(process.env.PORT) : 3000, 
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'http://localhost:10000', // Local testing ke liye
         changeOrigin: true,
       },
       '/socket.io': {
-        target: 'http://localhost:3001',
+        target: 'http://localhost:1000-', // Local testing ke liye
         ws: true,
         changeOrigin: true,
       },
     },
   },
+  // Preview block bhi add kar dete hain production ke liye
+  preview: {
+    host: '0.0.0.0',
+    port: process.env.PORT ? parseInt(process.env.PORT) : 3000,
+  }
 })
