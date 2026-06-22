361b1183b4d10ad26f8d6ca6ac2079e6960020ba
AMIT KUMAR SAH <amitkumarsahugupta@gmail.com>
2026-06-22 08:28:23 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,21 +6,14 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     allowedHosts: true, 
-    // Render standard ports use karta hai (jaise 10000)
     port: process.env.PORT ? parseInt(process.env.PORT) : 3000,
-    // Local proxy par depend na rahein, production me direct backend URL dein:
-const API = axios.create({
-  baseURL: 'https://open-wa-backent-1.onrender.com' 
-});
     proxy: {
       '/api': {
-        // Yahan aapke BACKEND ka URL aayega
         target: 'https://open-wa-backent-1.onrender.com',
         changeOrigin: true,
         secure: false,
       },
       '/socket.io': {
-        // Socket ke liye bhi BACKEND ka URL
         target: 'https://open-wa-backent-1.onrender.com',
         ws: true,
         changeOrigin: true,
